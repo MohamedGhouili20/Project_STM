0bf3438dd21044f78222199a6de54c748f786f01
MohamedGhouili20 <Mohamed.Ghouili@outlook.com>
2025-02-12 22:03:09 +0100

    Create math_operations.h

---
+++ math_operations.h
@@ -0,0 +1,10 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+// Déclaration des fonctions
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
+
+#endif // MATH_OPERATIONS_H
